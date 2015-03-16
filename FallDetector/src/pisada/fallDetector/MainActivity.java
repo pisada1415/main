bc1796524ec7a02c42fba9e8ba9fc68f35d17a82
Pisada1415 <piero.seminerio@alice.it>
2015-03-16 19:00:31 +0100

    prova3

--- FallDetector/src/pisada/fallDetector/MainActivity.java
+++ FallDetector/src/pisada/fallDetector/MainActivity.java
@@ -1,9 +1,8 @@
 package pisada.fallDetector;
 
+import pisada.falldetector.R;
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
