1940da94ac18dfdd625865f7848839f0262eb699
Pisada1415 <piero.seminerio@alice.it>
2015-03-17 19:48:33 +0100

    modifica main

--- FallDetector/src/pisada/fallDetector/MainActivity.java
+++ FallDetector/src/pisada/fallDetector/MainActivity.java
@@ -2,6 +2,7 @@ package pisada.fallDetector;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView txt = (TextView) findViewById(R.id.txt);
+        txt.setText("Merdaaaaaaaaaaaaaaaaa");
     }
 }
