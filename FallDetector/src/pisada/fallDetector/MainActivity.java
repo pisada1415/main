0451253120505ce25779b7517f85b9db71242fce
Samuele <samuele8@gmail.com>
2015-03-18 08:28:28 +0100

    ditemi se qualcuno non ha pushato lì

--- FallDetector/src/pisada/fallDetector/MainActivity.java
+++ FallDetector/src/pisada/fallDetector/MainActivity.java
@@ -2,7 +2,9 @@ package pisada.fallDetector;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -10,8 +12,17 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView txt = (TextView) findViewById(R.id.txt);
+        TextView txt = (TextView) findViewById(R.id.txt); //qualcuno non ha pushato il layout? mi da errore qui.. :/
         txt.setText("Merdaaaaaaaaaaaaaaaaa");
         //culooooo
+        txt.setClickable(true);
+        txt.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Toast.makeText(MainActivity.this, "BESTIAAAA", Toast.LENGTH_LONG).show();;
+			}
+		});
     }
 }
