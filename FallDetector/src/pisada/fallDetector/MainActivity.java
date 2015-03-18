33b8c52eaab499395d81d182c48845958cb54ee8
daniel91gn <daniel91gn@gmail.com>
2015-03-18 12:19:41 +0100

    skldfgpi

--- FallDetector/src/pisada/fallDetector/MainActivity.java
+++ FallDetector/src/pisada/fallDetector/MainActivity.java
@@ -13,5 +13,7 @@ public class MainActivity extends Activity {
         TextView txt = (TextView) findViewById(R.id.txt);
         txt.setText("Merdaaaaaaaaaaaaaaaaa");
         //culooooo
+        
+        //vkfjbvoivwoi
     }
 }
