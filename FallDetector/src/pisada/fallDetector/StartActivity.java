9db2bba4c1a0506fc7db4186d1c70119bbaa856a
Samuele <samuele8@gmail.com>
2015-03-18 10:57:43 +0100

    Ho creato/rinominato le classi principali

---
+++ FallDetector/src/pisada/fallDetector/StartActivity.java
@@ -0,0 +1,16 @@
+package pisada.fallDetector;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class StartActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
