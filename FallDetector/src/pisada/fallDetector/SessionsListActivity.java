1806e4f4e7c2f126a7238033b6568862b790bca7
daniel91gn <daniel91gn@gmail.com>
2015-03-23 18:32:46 +0100

    Schiacciate il pulsante

--- FallDetector/src/pisada/fallDetector/SessionsListActivity.java
+++ FallDetector/src/pisada/fallDetector/SessionsListActivity.java
@@ -28,9 +28,14 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.ScaleAnimation;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
@@ -39,7 +44,7 @@ import android.support.v7.widget.RecyclerView;
 
 
 public class SessionsListActivity extends ActionBarActivity implements SensorEventListener {
-	
+
 	private RecyclerView rView;
 	private CardAdapter cardAdapter;
 	private RecyclerView.LayoutManager mLayoutManager;
@@ -60,52 +65,52 @@ public class SessionsListActivity extends ActionBarActivity implements SensorEve
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_sessions_list);
-		
+
 		//INIZIALIZZO RECYCLERVIEW
-	
-		
+
+
 		rView=(RecyclerView) findViewById(R.id.my_recycler_view);
 		rView.setHasFixedSize(true);
 		cardAdapter=new CardAdapter(acquisitions, this);
 		rView.setAdapter(cardAdapter);
 		mLayoutManager = new LinearLayoutManager(this);
 		rView.setLayoutManager(mLayoutManager);
-	
+
 		//APRO CONNESSIONI AL DATABASE
 		sessionData=new SessionDataSource(this);
 		sessionData.open();
 		acquisitionData=new AcquisitionDataSource(this);
 		acquisitionData.open();
-		
+
 		//PROVO A FARE INSERT DELL'UNICA SESSIONE
 		try{sessionData.storeNewSession(sessionName, "noFoto",System.currentTimeMillis(), 0);
 		}
 		catch(Exception e){
 			e.printStackTrace();
 		}
-		
+
 		//INIZIALIZZO SENSORE E MANAGER
 		mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 		mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 		mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
-		
+
 		//RIEMPIO RECYCLERVIEW CON TUTTE LE ACQUISIZIONI
 		acquisitions=acquisitionData.acquisitions(sessionName);
 		for(Acquisition a: acquisitions){
 			cardAdapter.addItem(a);
 		}
-		
+
 	}
-	
-	
+
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.sessions_list, menu);
 		return true;
 	}
-	
-	
+
+
 	@Override
 	public void onResume(){
 		super.onResume();
@@ -114,8 +119,8 @@ public class SessionsListActivity extends ActionBarActivity implements SensorEve
 		acquisitionData.open();
 	}
 
-	
-	
+
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		// Handle action bar item clicks here. The action bar will
@@ -126,8 +131,8 @@ public class SessionsListActivity extends ActionBarActivity implements SensorEve
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
-		
-		
+
+
 	}
 	@Override
 	protected void onPause() {
@@ -141,7 +146,7 @@ public class SessionsListActivity extends ActionBarActivity implements SensorEve
 	@Override
 	public void onSensorChanged(SensorEvent event) {
 		long currentTime=System.currentTimeMillis();
-	/*	TextView xText=(TextView) findViewById(R.id.xAxis);
+		/*	TextView xText=(TextView) findViewById(R.id.xAxis);
 		TextView yText=(TextView) findViewById(R.id.yAxis);
 		TextView zText=(TextView) findViewById(R.id.zAxis);*/
 		float xValue=event.values[0];
@@ -163,23 +168,6 @@ public class SessionsListActivity extends ActionBarActivity implements SensorEve
 
 			}
 		}
-	
-		//SCRiVE NELLE TEXT VIEW 
-	/*	if((currentTime-lastFall)/1000>4) fall=false;
-		if(!fall){
-			if(absG<30){
-				xText.setText(Float.toString(xValue));
-				yText.setText(Float.toString(yValue));
-				zText.setText(Float.toString(zValue));
-			}
-			else{
-				xText.setText("FAAAAALL! reached over "+(int)absG+" qualcosa");
-				yText.setText("FAAAAALL! reached over "+(int)absG+" qualcosa");
-				zText.setText("FAAAAALL! reached over "+(int)absG+" qualcosa");
-				fall=true;
-				lastFall=currentTime;
-			}
-		}*/
 	}
 
 	@Override
@@ -187,6 +175,16 @@ public class SessionsListActivity extends ActionBarActivity implements SensorEve
 		// TODO Auto-generated method stub
 
 	}
+	public void espandi(View v){
+		ImageView img=(ImageView) findViewById(R.id.image);
+		CardView card=(CardView) img.getParent();
+		ScaleAnimation anim = new ScaleAnimation(1, 1, 1, 0);
+		anim.setDuration(2000);
+		card.startAnimation(anim);
+	}
+
+
+
 
 }
 
